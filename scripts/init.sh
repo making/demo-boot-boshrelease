ade3fb0179c6a61b6582a377dbee139786660833
making <makingx@gmail.com>
2016-02-10 11:34:16 +0900

    Change to use blob

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,8 +3,15 @@
 echo "==== Update submodules  ===="
 git submodule update --init --recursive
 
-echo "==== Download JDK ===="
+echo "==== Prepare Blob (java) ===="
 pushd packages/java
 sh download-and-add-blob.sh
+popd
+
+echo "==== Prepare Blob (demo) ===="
+pushd src/demo
+./mvnw clean package -Dmaven.test.skip=true
+cd ../..
+bosh add blob src/demo/target/demo-0.0.1.RELEASE.jar demo
 popd
 
--- scripts/prebuild.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-echo "== pre-build demo =="
-pushd src/demo
-./mvnw clean package -Dmaven.test.skip=true
-popd
